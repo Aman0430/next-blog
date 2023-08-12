8b2ddc4c23fb3ef5415de94df00fd87a13b925b0
Aman0430 <112618669+Aman0430@users.noreply.github.com>
2023-08-12 20:04:01 +0530

    feat: api integration with UI

---
+++ app/blogs/addBlogs/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { Fragment, useRef } from "react";
+import { Toaster, toast } from "react-hot-toast";
+
+const AddBlogs = () => {
+  const titleRef = useRef<HTMLInputElement | null>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const postBlog = async ({
+    title,
+    description,
+  }: {
+    title: String;
+    description: String;
+  }) => {
+    const res = fetch("http://localhost:3000/api/blog", {
+      method: "POST",
+      body: JSON.stringify({ title, description }),
+      //@ts-ignore
+      "Content-Type": "application/json",
+    });
+    return (await res).json();
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if (titleRef.current && descriptionRef.current) {
+      toast.loading("Sending Request", { id: "1" });
+      await postBlog({
+        title: titleRef.current?.value,
+        description: descriptionRef.current?.value,
+      });
+      toast.success("Good to Go...", { id: "1" });
+    }
+  };
+
+  return (
+    <Fragment>
+      <Toaster />
+      <div className="w-full m-auto flex my-4">
+        <div className="flex flex-col justify-center items-center m-auto">
+          <p className="text-2xl text-slate-200 font-bold p-3">Add Blog</p>
+
+          <form onSubmit={handleSubmit}>
+            <input
+              ref={titleRef}
+              placeholder="Enter Title"
+              type="text"
+              className="rounded-md px-4 w-full py-2 my-2 text-slate-700"
+            />
+            <textarea
+              ref={descriptionRef}
+              placeholder="Enter Description"
+              className="rounded-md px-4 py-2 w-full my-2 text-slate-700"
+            ></textarea>
+            <button className="font-semibold px-4 py-2 shadow-xl bg-slate-300 rounded-md m-auto hover:bg-gray-400 transition">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default AddBlogs;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,63 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
-  return <div>Hello</div>;
+async function fetchBlogs() {
+  let res = await fetch("http://localhost:3000/api/blog", {
+    next: { revalidate: 10 },
+  });
+
+  const data = await res.json();
+  return data.posts;
+}
+
+export default async function Home() {
+  const posts = await fetchBlogs();
+  // console.log(posts);
+  return (
+    <main className="w-full h-full">
+      <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-4 sm:rounded-lg bg-gray-800 drop-shadow-lg mt-0 sm:mt-4">
+        <h1 className="text-slate-200 text-center text-2xl font-extrabold font-[verdana]">
+          Share your Blog all over the{" "}
+          <span className="text-red-800">World</span>.
+        </h1>
+      </div>
+
+      <div className="flex mb-6">
+        <Link
+          href={"/blogs/addBlogs"}
+          className="md:w-1/6 sm:w-2/4 text-center rounded-md p-2 m-auto bg-gray-400 font-semibold"
+        >
+          Add New Blog 🚀
+        </Link>
+      </div>
+
+      <div className="w-full flex  flex-col justify-center items-center">
+        {posts?.map((post: any) => (
+          <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-gray-500 flex flex-col justify-center">
+            <div className="flex items-center my-3">
+              <div className="mr-auto">
+                <h2 className="mr-auto font-semibold">{post.title}</h2>
+              </div>
+              <div className="rounded-full bg-gray-800">
+                <Link
+                  href={`/blogs/edit/${post.id}`}
+                  className="px-4 py-1  text-center text-xl font-semibold text-slate-200"
+                >
+                  ✏️
+                </Link>
+              </div>
+            </div>
+
+            <div className="mr-auto my-1">
+              <blockquote className="font-bold text-slate-700">
+                {new Date(post.date).toDateString()}
+              </blockquote>
+            </div>
+            <div className=" mr-auto my-1">
+              <h2>{post.description}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
